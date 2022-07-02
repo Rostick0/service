1fa5a990e3d712476e8ff22fe29d98c539992ac1
Rostislav <rostik057@gmail.com>
2022-07-02 22:25:33 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-const throttle = (func, ms) => { // объявляем функцию throttle
+// глобальные функции
 
-    let locked = false // переменная которая отвечает за блокировку вызова функции
+const throttle = (func, ms) => {
 
-    return function() { // эта функция запускается при каждом событии движения курсора
+    let locked = false
 
-      if (locked) return // если заблокировано, то прекращаем выполнение этой функции
+    return function() {
 
-      const context = this // запоминаем передаваемую функцию func
-      const args = arguments // запоминаем параметры передаваемой функции func
+      if (locked) return
 
-      locked = true // блокируем вызов функции
+      const context = this
+      const args = arguments
 
-      setTimeout(() => { // устанавливаем время ожидания
+      locked = true
 
-        func.apply(context, args) // выполняем переданную функцию func
-        locked = false // снимаем блокировку
+      setTimeout(() => {
 
-      }, ms) // подставляем значение параметра ms
+        func.apply(context, args)
+        locked = false
+
+      }, ms)
 
     }
 }
@@ -35,9 +37,7 @@ if (navigationLinks) {
             
             if (navigationLink.dataset.goto && document.querySelector(navigationLink.dataset.goto)) {
                 const gotoBlock = document.querySelector(navigationLink.dataset.goto);
-                const gotoBlockValue = (gotoBlock.getBoundingClientRect().top + pageYOffset)*0.99;
-                console.log(gotoBlockValue)
-            
+                const gotoBlockValue = (gotoBlock.getBoundingClientRect().top + pageYOffset)*0.99;         
 
                 // if (burgerMenu.classList.contains('_active')) {
                 //     removeActive(burgerButton);
@@ -60,17 +60,13 @@ if (navigationLinks) {
 
 
 
-// стрелка для скролла
+// стрелка для скролла до верха
 
 const arrowToTop = document.querySelector('.arrow__to-top');
 
 if (arrowToTop) {
-    function scrollTodsa() {
-        console.log(pageYOffset);
-    }
 
     window.addEventListener('scroll', throttle(function() {
-        console.log(pageYOffset);
         if (pageYOffset >= 800) {
             arrowToTop.classList.remove('_no-active');
             return;
@@ -91,6 +87,116 @@ if (arrowToTop) {
 
 
 
-// модальное окно
+// активаця модального окно
 
-const modal = document.querySelector('.modal');
+(() => {
+    const buttonTelephone = document.querySelectorAll('.button__telephone');
+    const servicesListPrice = document.querySelectorAll('.services__list_price');
+
+    const modal = document.querySelector('.modal');
+
+    function createEvent() {
+        const modalButton = document.querySelector('.modal__button');
+
+        activeAnimation();
+
+        modal.addEventListener('click', e => {
+            if (e.target.classList[0] == 'modal') {
+                removeModal();
+            }
+        })
+
+        modalButton.addEventListener('click', async e => {
+            const modalInputName = document.querySelector('.modal__input_name').value.trim();
+            const modalInputTelephone = document.querySelector('.modal__input_telephone').value.trim();
+            const modalInputText = document.querySelector('.modal__input_text').value.trim();
+
+            const data = {
+                name: modalInputName,
+                telephone: modalInputTelephone,
+                text: modalInputText
+            };
+
+            console.log(JSON.stringify(data))
+
+            removeModal();
+
+            // return await fetch('', {
+            //     method: 'POST',
+            //     body: JSON.stringify(data)
+            // })
+        })
+    }
+
+    function activeAnimation() {
+        modal.style.top = '0';
+        modal.style.animation = 'blackout-modal 1.5s forwards';
+        document.body.classList.add('overflow-y-hidden');
+    }
+
+    function removeModal() {
+        modal.style.animation = '';
+        modal.style.top = '-100%';
+        document.body.classList.remove('overflow-y-hidden');
+    }
+
+    if (buttonTelephone) {
+        buttonTelephone.forEach(button => {
+            button.addEventListener('click', e => createEvent())
+        })
+    }
+
+    if (servicesListPrice) {
+        servicesListPrice.forEach(button => {
+            button.addEventListener('click', e => createEvent())
+        })
+    }
+})()
+
+
+
+
+// слайдер
+
+const serviceBrand = document.querySelector('.service-brand');
+
+if (serviceBrand) {
+    // const sliderArrowLeft = serviceBrand.querySelector('.slider__arrow_left');
+    // const sliderArrowRight = serviceBrand.querySelector('.slider__arrow_right');
+
+    // const serviceBrandSliderItems = document.querySelectorAll('.service-brand_slider_item');
+
+    let counter = 0;
+
+    new Swiper('.service-brand_slider_container', {
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            480: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1280: {
+                slidesPerView: 5,
+            }
+        },
+        slidesPerView: 5,
+        spaceBetween: 45,
+        loop: true,
+        lazy: {
+            loadOnTransitionStart: true,
+            loadPrevNext: true
+        },
+        preloadImages: true,
+        watchSlidesProgress: true,
+        watchSlidesVisibility: false,
+        navigation: {
+            nextEl: '.slider__arrow_right',
+            prevEl: '.slider__arrow_left'
+        }
+    })
+  
+}
